85ea10bd22b2b7636ba4c57e8fd0e3483c84e67d
Damien Laqueuvre <damien.laqueuvre@gmail.com>
2023-12-04 09:15:51 +0100

    Commenting and restructuring

--- extract_synonyms.py
+++ extract_synonyms.py
@@ -34,7 +34,7 @@ def generate_wordnet_format(word_id:int, syn_num:int, word:str, pos:str) ->str :
 
     Args:
         word_id (int): the synset ID 
-        syn_num (int): number of synonym
+        syn_num (int): count of synonym
         word (str): the word
         pos (str): pos
 
@@ -44,6 +44,18 @@ def generate_wordnet_format(word_id:int, syn_num:int, word:str, pos:str) ->str :
     return f"s({word_id}, {syn_num}, '{word}', {pos}, 1, 0).\n"
 
 def generate_synset(syn_id:int, syn_list:list, stop_words:list, pos:str, lemma_c:str) -> str: 
+    """Generate the wordnet list
+
+    Args:
+        syn_id (int): the synset ID 
+        syn_list (list): list of synonyms
+        stop_words (list): list of stopwords
+        pos (str): 'a' 'b' 'v' '?' of TOFINISH
+        lemma_c (str): word from wich the synset come form
+
+    Returns:
+        str: paragraph of the synset
+    """
     already_put = []
     synset = ""
     c=1
@@ -95,9 +107,12 @@ def syn_to_be_removed(syn, syn_list) :
 
 
 
-def extract_synonyms_to_file(stop_words=[]):
-    lang = 'fra'
+def extract_synonyms_to_file(lang:str="fra", stop_words:bool=True):
     word_id = 1
+    stop_words=[]
+    if(stop_words) : 
+       stop_words = open(f"stopword/stopword_{fra}.txt").read().splitlines()
+    
     with open(f'output/{lang}_synonyms_wordnet_format.txt', 'w', encoding='utf-8') as file:
         for lemma in wn.all_lemma_names(lang=lang):
             if lemma not in stop_words:
@@ -111,6 +126,6 @@ def extract_synonyms_to_file(stop_words=[]):
                     word_id += 1
                     
 #test_to_be_removed()
-stop_words = open("stopword/stopword_fr.txt").read().splitlines()
+
 #['le', 'la', 'de', 'et', 'à', 'est']
-extract_synonyms_to_file(stop_words)
+extract_synonyms_to_file()
